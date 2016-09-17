fbf0a49d3a77868c84c60b4849fc2cce508dcc17
Patrick Allen <allenpatrick86@gmail.com>
2016-09-17 10:02:09 -0700

    add notes

--- src/app.js
+++ src/app.js
@@ -4,7 +4,10 @@ const express = require('express');
 
 let app = express();
 
-app.get('/', function(request, response)); 
-//location parameter, annonymous callback function
+app.get('/', function(request, response){
+  response.send("I love learning!")
+
+});
+//function takes (location parameter, annonymous callback function)
 
 app.listen(3000);
