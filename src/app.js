2fc47a3080348db3d5bdb0a527fdba75d37d2307
Patrick Allen <allenpatrick86@gmail.com>
2016-09-17 12:50:04 -0700

    add styling and additional features

--- src/app.js
+++ src/app.js
@@ -3,10 +3,16 @@
 const express = require('express'),
         posts = require('./mock/posts.json');
 
+// Below converts object into an array.
+const postsLists = Object.keys(posts).map(function(value){
+                     return posts[value]
+                   });
+
 let app = express();
 
 app.use('/static', express.static(__dirname + '/public'));
 // the static statements adds that as a leading directory
+
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/templates'); //sets the path relative to the file rather than launch point
@@ -20,7 +26,7 @@ app.get('/blog/:title?', (req, res) => {
   const title = req.params.title;
   if (title === undefined) {
     res.status(503);
-    res.send("This page is under construction")
+    res.render('blog', { posts: postsLists});
   } else {
     const post = posts[title] || {};
     // The 'or' statement above here is a cheap way of avoiding writing explicit error handling. Use sparingly.
