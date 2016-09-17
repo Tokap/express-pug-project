4b3d1f0cdd21a89e60b2992107846f6124cee8d4
Patrick Allen <allenpatrick86@gmail.com>
2016-09-17 10:11:24 -0700

    add mock data and minor adjustments

--- src/app.js
+++ src/app.js
@@ -4,10 +4,16 @@ const express = require('express');
 
 let app = express();
 
-app.get('/', function(request, response){
-  response.send("I love learning!")
-
+app.get('/', (req, res) => {
+  res.send("<h1>I love learning!</h1>")
 });
 //function takes (location parameter, annonymous callback function)
 
-app.listen(3000);
+app.get('/blog', (req, res) => {
+
+});
+
+app.listen(3000, () =>{
+  console.log("The frontend server is running on port 3000")
+});
+// listen can take a callback as its second parameter
